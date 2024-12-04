f74705eece7fdfafbd3819d6f1cdeef2c204787c
vkh15088 <34000074+vkh15088@users.noreply.github.com>
2024-12-04 10:18:06 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  
   runApp(const MyApp());
 }
 
