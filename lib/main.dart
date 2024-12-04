989f549f4464bcf7d647e16fc740bae949c82cc6
vkh15088 <cuti2668@gmail.com>
2024-12-04 13:50:53 +0700

    Update tag

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  
   runApp(const MyApp());
 }
 
